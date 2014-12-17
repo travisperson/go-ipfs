6cea0649e58d90e228eddefcd20e9cd89b076383
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-12-16 17:00:02 -0800

    copy dirs
    License: MIT
    Signed-off-by: Brian Tiger Chow <brian@perfmode.com>

--- dockertest/bin/save_logs.sh
+++ dockertest/bin/save_logs.sh
@@ -6,3 +6,7 @@ docker logs dockertest_bootstrap_1 2>&1 | eval $STRIP > ./build/bootstrap.log
 docker logs dockertest_client_1 2>&1 | eval $STRIP > ./build/client.log 
 docker logs dockertest_data_1 2>&1 | eval $STRIP > ./build/data.log 
 docker logs dockertest_server_1 2>&1 | eval $STRIP > ./build/server.log
+
+docker cp dockertest_server_1:/root/.go-ipfs/logs/events.log    ./build/server-events.log
+docker cp dockertest_bootstrap_1:/root/.go-ipfs/logs/events.log ./build/bootstrap-events.log
+docker cp dockertest_client_1:/root/.go-ipfs/logs/events.log    ./build/client-events.log
