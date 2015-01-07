bc56822ea092d8ab891a63052b0aafd2bf0c40bc
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-01-07 16:31:05 -0500

    wip move

--- core/configuration.go
+++ core/configuration.go
@@ -3,59 +3,131 @@ package core
 // important to keep as an interface to allow implementations to vary
 
 import (
-	"io"
+	"errors"
+	"fmt"
 
 	context "github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+	b58 "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-base58"
 	datastore "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
 	sync "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore/sync"
+	ma "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-multiaddr"
 	blockstore "github.com/jbenet/go-ipfs/blocks/blockstore"
+	"github.com/jbenet/go-ipfs/config"
 	core_testutil "github.com/jbenet/go-ipfs/core/testutil"
 	diag "github.com/jbenet/go-ipfs/diagnostics"
 	exchange "github.com/jbenet/go-ipfs/exchange"
 	bitswap "github.com/jbenet/go-ipfs/exchange/bitswap"
 	bsnet "github.com/jbenet/go-ipfs/exchange/bitswap/network"
-	importer "github.com/jbenet/go-ipfs/importer"
-	chunk "github.com/jbenet/go-ipfs/importer/chunk"
 	namesys "github.com/jbenet/go-ipfs/namesys"
+	ic "github.com/jbenet/go-ipfs/p2p/crypto"
 	host "github.com/jbenet/go-ipfs/p2p/host"
+	p2pbhost "github.com/jbenet/go-ipfs/p2p/host/basic"
+	"github.com/jbenet/go-ipfs/p2p/net/swarm"
 	peer "github.com/jbenet/go-ipfs/p2p/peer"
-	path "github.com/jbenet/go-ipfs/path"
 	dht "github.com/jbenet/go-ipfs/routing/dht"
-	uio "github.com/jbenet/go-ipfs/unixfs/io"
-	util "github.com/jbenet/go-ipfs/util"
 	"github.com/jbenet/go-ipfs/util/datastore2"
 	ds2 "github.com/jbenet/go-ipfs/util/datastore2"
+	"github.com/jbenet/go-ipfs/util/debugerror"
 	delay "github.com/jbenet/go-ipfs/util/delay"
 )
 
-func (c *IpfsNode) ID() peer.ID {
-	return c.repo.ID()
-}
+// mock network, dht, bitswap
+// p2p network, grandcentral, bitswap
+//
 
-func (c *IpfsNode) Bootstrap(ctx context.Context, p peer.ID) error {
-	return c.repo.Bootstrap(ctx, p)
-}
+func Online(cfg *config.Config) Config {
+	// TODO load private key
+	return func(ctx context.Context) (Configuration, error) {
+		if cfg == nil {
+			return nil, debugerror.Errorf("configuration required")
+		}
+		peerID := peer.ID(b58.Decode(cfg.Identity.PeerID))
+		privateKey, err := loadPrivateKey(&cfg.Identity, peerID)
+		if err != nil {
+			return nil, err
+		}
+		datastore, err := makeDatastore(cfg.Datastore)
+		if err != nil {
+			return nil, err
+		}
+		blockstore, err := blockstore.WriteCached(blockstore.NewBlockstore(datastore), kSizeBlockstoreWriteCache)
+		if err != nil {
+			return nil, err
+		}
 
-func (c *IpfsNode) Cat(k util.Key) (io.Reader, error) {
-	catterdag := c.DAG
-	nodeCatted, err := (&path.Resolver{catterdag}).ResolvePath(k.String())
-	if err != nil {
-		return nil, err
+		peerstore := peer.NewPeerstore()
+		peerstore.AddPrivKey(peerID, privateKey)
+		listenAddrs, err := listenAddresses(cfg)
+		if err != nil {
+			return nil, debugerror.Wrap(err)
+		}
+		network, err := swarm.NewNetwork(ctx, listenAddrs, peerID, peerstore)
+		if err != nil {
+			return nil, debugerror.Wrap(err)
+		}
+		// TODO consider giving this function a context group so the components can be added n.AddChildGroup(network.CtxGroup())
+		peerHost := p2pbhost.New(network)
+
+		// Explicitly set these as our listen addrs.
+		// Q: Why not do it inside inet.NewNetwork?
+		// A: Because this way we can listen on addresses without necessarily
+		//    advertising those publicly.
+		addrs, err := peerHost.Network().InterfaceListenAddresses()
+		if err != nil {
+			return nil, debugerror.Wrap(err)
+		}
+		peerstore.AddAddresses(peerID, addrs)
+
+		diagnosticService := diag.NewDiagnostics(peerID, peerHost)
+
+		dhtRouting := dht.NewDHT(ctx, peerHost, datastore)
+		dhtRouting.Validators[IpnsValidatorTag] = namesys.ValidateIpnsRecord
+		// TODO n.AddChildGroup(dhtRouting)
+
+		const alwaysSendToPeer = true // use YesManStrategy
+		bitswapNetwork := bsnet.NewFromIpfsHost(peerHost, dhtRouting)
+		bitswapExchange := bitswap.New(ctx, peerID, bitswapNetwork, blockstore, alwaysSendToPeer)
+
+		// TODO implement an offline namesys that serves only local names.
+		nsys := namesys.NewNameSystem(dhtRouting)
+
+		// TODO consider moving connection supervision into the Network. We've
+		// discussed improvements to this Node constructor. One improvement
+		// would be to make the node configurable, allowing clients to inject
+		// an Exchange, Network, or Routing component and have the constructor
+		// manage the wiring. In that scenario, this dangling function is a bit
+		// awkward.
+		go superviseConnections(ctx, peerHost, dhtRouting, peerstore, cfg.Bootstrap)
+		return &configuration{
+			online:           true,
+			exchange:         bitswapExchange,
+			diagnoticService: diagnosticService,
+			nameSystem:       nsys,
+		}, errors.New("TODO")
 	}
-	return uio.NewDagReader(nodeCatted, catterdag)
 }
 
-func (c *IpfsNode) Add(r io.Reader) (util.Key, error) {
-	nodeAdded, err := importer.BuildDagFromReader(
-		r,
-		c.DAG,
-		nil,
-		chunk.DefaultSplitter,
-	)
-	if err != nil {
-		return "", err
+func Offline(cfg *config.Config) Config {
+	// offline exchange
+	return func(context.Context) (Configuration, error) {
+		if cfg == nil {
+			return nil, debugerror.Errorf("configuration required")
+		}
+		return nil, errors.New("TODO")
 	}
-	return nodeAdded.Key()
+}
+
+type Configuration interface {
+	ID() peer.ID
+	Exchange() exchange.Interface
+	OnlineMode() bool
+	Peerstore() peer.Peerstore
+	Blockstore() blockstore.Blockstore
+	Datastore() ds2.ThreadSafeDatastoreCloser
+	// TODO if configID == "" { return debugerror.New("Identity was not set in config (was ipfs init run?)") }
+	// TODO if len(configID) == 0 { return debugerror.New("No peer ID in config! (was ipfs init run?)") }
+
+	Bootstrap(ctx context.Context, peer peer.ID) error
 }
 
 type Config func(ctx context.Context) (Configuration, error)
@@ -109,4 +181,37 @@ func MocknetTestRepo(p peer.ID, h host.Host, conf core_testutil.LatencyConfig) C
 			id:             p,
 		}, nil
 	}
+}
+
+func loadPrivateKey(cfg *config.Identity, id peer.ID) (ic.PrivKey, error) {
+	sk, err := cfg.DecodePrivateKey("passphrase todo!")
+	if err != nil {
+		return nil, err
+	}
+
+	id2, err := peer.IDFromPrivateKey(sk)
+	if err != nil {
+		return nil, err
+	}
+
+	if id2 != id {
+		return nil, fmt.Errorf("private key in config does not match id: %s != %s", id, id2)
+	}
+
+	return sk, nil
+}
+
+func listenAddresses(cfg *config.Config) ([]ma.Multiaddr, error) {
+
+	var err error
+	listen := make([]ma.Multiaddr, len(cfg.Addresses.Swarm))
+	for i, addr := range cfg.Addresses.Swarm {
+
+		listen[i], err = ma.NewMultiaddr(addr)
+		if err != nil {
+			return nil, fmt.Errorf("Failure to parse config.Addresses.Swarm[%d]: %s", i, cfg.Addresses.Swarm)
+		}
+	}
+
+	return listen, nil
 }
--- core/core.go
+++ core/core.go
@@ -1,34 +1,29 @@
 package core
 
 import (
-	"errors"
-	"fmt"
+	"io"
 
+	uio "github.com/jbenet/go-ipfs/unixfs/io"
 	context "github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
-	b58 "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-base58"
 	ctxgroup "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-ctxgroup"
-	ma "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-multiaddr"
 	bstore "github.com/jbenet/go-ipfs/blocks/blockstore"
 	bserv "github.com/jbenet/go-ipfs/blockservice"
 	config "github.com/jbenet/go-ipfs/config"
 	diag "github.com/jbenet/go-ipfs/diagnostics"
 	exchange "github.com/jbenet/go-ipfs/exchange"
-	bitswap "github.com/jbenet/go-ipfs/exchange/bitswap"
-	bsnet "github.com/jbenet/go-ipfs/exchange/bitswap/network"
 	mount "github.com/jbenet/go-ipfs/fuse/mount"
+	importer "github.com/jbenet/go-ipfs/importer"
+	chunk "github.com/jbenet/go-ipfs/importer/chunk"
 	merkledag "github.com/jbenet/go-ipfs/merkledag"
 	namesys "github.com/jbenet/go-ipfs/namesys"
 	ic "github.com/jbenet/go-ipfs/p2p/crypto"
 	p2phost "github.com/jbenet/go-ipfs/p2p/host"
-	p2pbhost "github.com/jbenet/go-ipfs/p2p/host/basic"
-	swarm "github.com/jbenet/go-ipfs/p2p/net/swarm"
 	peer "github.com/jbenet/go-ipfs/p2p/peer"
 	path "github.com/jbenet/go-ipfs/path"
 	pin "github.com/jbenet/go-ipfs/pin"
 	routing "github.com/jbenet/go-ipfs/routing"
-	dht "github.com/jbenet/go-ipfs/routing/dht"
+	util "github.com/jbenet/go-ipfs/util"
 	ds2 "github.com/jbenet/go-ipfs/util/datastore2"
-	debugerror "github.com/jbenet/go-ipfs/util/debugerror"
 	eventlog "github.com/jbenet/go-ipfs/util/eventlog"
 )
 
@@ -77,110 +72,12 @@ type Mounts struct {
 }
 
 // NewIpfsNode constructs a new IpfsNode based on the given config.
+// Deprecated.
 func NewIpfsNode(ctx context.Context, cfg *config.Config, online bool) (*IpfsNode, error) {
 	if online {
 		return New(ctx, Online(cfg))
 	}
 	return New(ctx, Offline(cfg))
-}
-
-// mock network, dht, bitswap
-// p2p network, grandcentral, bitswap
-//
-
-func Online(cfg *config.Config) Config {
-	// TODO load private key
-	return func(ctx context.Context) (Configuration, error) {
-		if cfg == nil {
-			return nil, debugerror.Errorf("configuration required")
-		}
-		peerID := peer.ID(b58.Decode(cfg.Identity.PeerID))
-		privateKey, err := loadPrivateKey(&cfg.Identity, peerID)
-		if err != nil {
-			return nil, err
-		}
-		datastore, err := makeDatastore(cfg.Datastore)
-		if err != nil {
-			return nil, err
-		}
-		blockstore, err := bstore.WriteCached(bstore.NewBlockstore(datastore), kSizeBlockstoreWriteCache)
-		if err != nil {
-			return nil, err
-		}
-
-		peerstore := peer.NewPeerstore()
-		peerstore.AddPrivKey(peerID, privateKey)
-		listenAddrs, err := listenAddresses(cfg)
-		if err != nil {
-			return nil, debugerror.Wrap(err)
-		}
-		network, err := swarm.NewNetwork(ctx, listenAddrs, peerID, peerstore)
-		if err != nil {
-			return nil, debugerror.Wrap(err)
-		}
-		// TODO consider giving this function a context group so the components can be added n.AddChildGroup(network.CtxGroup())
-		peerHost := p2pbhost.New(network)
-
-		// Explicitly set these as our listen addrs.
-		// Q: Why not do it inside inet.NewNetwork?
-		// A: Because this way we can listen on addresses without necessarily
-		//    advertising those publicly.
-		addrs, err := peerHost.Network().InterfaceListenAddresses()
-		if err != nil {
-			return nil, debugerror.Wrap(err)
-		}
-		peerstore.AddAddresses(peerID, addrs)
-
-		diagnosticService := diag.NewDiagnostics(peerID, peerHost)
-
-		dhtRouting := dht.NewDHT(ctx, peerHost, datastore)
-		dhtRouting.Validators[IpnsValidatorTag] = namesys.ValidateIpnsRecord
-		// TODO n.AddChildGroup(dhtRouting)
-
-		const alwaysSendToPeer = true // use YesManStrategy
-		bitswapNetwork := bsnet.NewFromIpfsHost(peerHost, dhtRouting)
-		bitswapExchange := bitswap.New(ctx, peerID, bitswapNetwork, blockstore, alwaysSendToPeer)
-
-		// TODO implement an offline namesys that serves only local names.
-		nsys := namesys.NewNameSystem(dhtRouting)
-
-		// TODO consider moving connection supervision into the Network. We've
-		// discussed improvements to this Node constructor. One improvement
-		// would be to make the node configurable, allowing clients to inject
-		// an Exchange, Network, or Routing component and have the constructor
-		// manage the wiring. In that scenario, this dangling function is a bit
-		// awkward.
-		go superviseConnections(ctx, peerHost, dhtRouting, peerstore, cfg.Bootstrap)
-		return &configuration{
-			online:           true,
-			exchange:         bitswapExchange,
-			diagnoticService: diagnosticService,
-			nameSystem:       nsys,
-		}, errors.New("TODO")
-	}
-}
-
-func Offline(cfg *config.Config) Config {
-	// offline exchange
-	return func(context.Context) (Configuration, error) {
-		if cfg == nil {
-			return nil, debugerror.Errorf("configuration required")
-		}
-		return nil, errors.New("TODO")
-	}
-}
-
-type Configuration interface {
-	ID() peer.ID
-	Exchange() exchange.Interface
-	OnlineMode() bool
-	Peerstore() peer.Peerstore
-	Blockstore() bstore.Blockstore
-	Datastore() ds2.ThreadSafeDatastoreCloser
-	// TODO if configID == "" { return debugerror.New("Identity was not set in config (was ipfs init run?)") }
-	// TODO if len(configID) == 0 { return debugerror.New("No peer ID in config! (was ipfs init run?)") }
-
-	Bootstrap(ctx context.Context, peer peer.ID) error
 }
 
 func New(parent context.Context, c Config) (*IpfsNode, error) {
@@ -235,35 +132,32 @@ func (n *IpfsNode) OnlineMode() bool {
 	return n.onlineMode
 }
 
-func loadPrivateKey(cfg *config.Identity, id peer.ID) (ic.PrivKey, error) {
-	sk, err := cfg.DecodePrivateKey("passphrase todo!")
-	if err != nil {
-		return nil, err
-	}
-
-	id2, err := peer.IDFromPrivateKey(sk)
-	if err != nil {
-		return nil, err
-	}
-
-	if id2 != id {
-		return nil, fmt.Errorf("private key in config does not match id: %s != %s", id, id2)
-	}
-
-	return sk, nil
+func (c *IpfsNode) ID() peer.ID {
+	return c.repo.ID()
 }
 
-func listenAddresses(cfg *config.Config) ([]ma.Multiaddr, error) {
+func (c *IpfsNode) Bootstrap(ctx context.Context, p peer.ID) error {
+	return c.repo.Bootstrap(ctx, p)
+}
 
-	var err error
-	listen := make([]ma.Multiaddr, len(cfg.Addresses.Swarm))
-	for i, addr := range cfg.Addresses.Swarm {
-
-		listen[i], err = ma.NewMultiaddr(addr)
-		if err != nil {
-			return nil, fmt.Errorf("Failure to parse config.Addresses.Swarm[%d]: %s", i, cfg.Addresses.Swarm)
-		}
+func (c *IpfsNode) Cat(k util.Key) (io.Reader, error) {
+	catterdag := c.DAG
+	nodeCatted, err := (&path.Resolver{catterdag}).ResolvePath(k.String())
+	if err != nil {
+		return nil, err
 	}
+	return uio.NewDagReader(nodeCatted, catterdag)
+}
 
-	return listen, nil
+func (c *IpfsNode) Add(r io.Reader) (util.Key, error) {
+	nodeAdded, err := importer.BuildDagFromReader(
+		r,
+		c.DAG,
+		nil,
+		chunk.DefaultSplitter,
+	)
+	if err != nil {
+		return "", err
+	}
+	return nodeAdded.Key()
 }
